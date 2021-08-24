65b15d30c026be6394e3c352d1c18c4b8cf2c97b
Nadia Savchuck <savchyk.n2003@gmail.com>
2021-08-24 16:32:09 +0300

    added instances of the class

--- main.js
+++ main.js
@@ -11,3 +11,17 @@ class Worker {
         return this.rate * this.days
     }
 }
+const worker1 = new Worker('Ivan', 'Ivanov', 10, 31);
+console.log(worker1.name);
+console.log(worker1.surname);
+console.log(worker1.rate);
+console.log(worker1.days);
+console.log(worker1.getSalary());
+
+const worker2 = new Worker('Petro', 'Ivanov', 12, 10);
+console.log(worker2.name);
+console.log(worker2.surname);
+console.log(worker2.rate);
+console.log(worker2.days);
+console.log(worker2.getSalary());
+console.log(sumSalary);
